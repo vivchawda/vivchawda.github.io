d06dc8a8005a14fadab44639cd55eab0cbd7d295
Viv <vivchawda@gmail.com>
2018-11-05 21:17:26 +0530

    Added d2020

---
+++ d2020/index.js
@@ -0,0 +1,7 @@
+var animation = lottie.loadAnimation({
+  container: document.getElementById('bm'), // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://raw.githubusercontent.com/vivchawda/lottie/master/data.json' // the path to the animation json
+});
